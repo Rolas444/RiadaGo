533ea2cc85c34791901ade1e844280e0cc176282
DESKTOP-PR8MKLN\hrodland <7200ws@gmail.com>
2023-03-08 19:15:20 -0500

    addded dark-theme

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,26 @@
 import { useState } from 'react'
 import './App.css'
+import { Helmet } from 'react-helmet'
 import Navbar from './commons/nav/navbar.component'
+import { useCommonStore } from './stores/commonStore'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { theme } = useCommonStore((state)=>({
+    theme: state.theme
+  }));
+  
 
   return (
-    <div className="container">
-      <Navbar/>
-    </div>
+    <>
+      <Helmet htmlAttributes={theme?{'data-theme':'light'}:{'data-theme':'dark'} } >
+        <title>Riada Go</title>
+      </Helmet>
+      <div className="container">
+
+        <Navbar />
+      </div>
+    </>
+
   )
 }
 
